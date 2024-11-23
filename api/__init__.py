1ab7d5e8a3c0b3d92df7c7e2d1ef6538f83d8e35
Nikolay Makovetsky <nicholasmakovetsky@gmail.com>
2024-11-23 16:38:34 +0300

    add logging

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from api.core.server import Server
 import uvicorn
+from api.core.logging import logger, LOG_CONFIG
 
 
 def application() -> FastAPI:
     app = FastAPI()
+    logger.info("==================== Start service =========================")
     return Server(app).get_app()
 
 
 if __name__ == '__main__':
-    uvicorn.run(application)
+    uvicorn.run(application, log_level="trace", log_config=LOG_CONFIG)
+    logger.info("==================== Stop service =========================")
---
+++ api/core/logging/__init__.py
@@ -0,0 +1,2 @@
+from .logger import logger
+from .log_config import LOG_CONFIG
