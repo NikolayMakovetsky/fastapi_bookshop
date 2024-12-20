4ff4402a130696159e900853b7d5e4bff1f7fd0c
NikolayMakovetsky <nicholasmakovetsky@gmail.com>
2024-12-20 14:56:23 +0300

    added Singleton in database.py +

--- api/database.py
+++ api/database.py
@@ -5,10 +5,28 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 
 from api.models import User
 
-# engine = create_async_engine("sqlite+aiosqlite:///bookshop_db.db")
-engine = create_async_engine("postgresql+asyncpg://postgres:postgres@localhost:5432/bookshop_db2", echo=True)
 
-new_session = async_sessionmaker(engine, expire_on_commit=False)
+# engine = create_async_engine("sqlite+aiosqlite:///bookshop_db.db")
+
+class Singleton:
+    instance = None
+
+    def __new__(cls):
+        if Singleton.instance is None:
+            Singleton.instance = super().__new__(cls)
+            Singleton._do_work(Singleton.instance)
+        return Singleton.instance
+
+    def _do_work(self):
+        engine = create_async_engine("postgresql+asyncpg://postgres:postgres@localhost:5432/bookshop_db2", echo=True)
+        db_session = async_sessionmaker(engine, expire_on_commit=False)
+        self.session = db_session
+
+
+new_session = Singleton().session
+
+# engine = create_async_engine("postgresql+asyncpg://postgres:postgres@localhost:5432/bookshop_db2", echo=True)
+# new_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
 # Две эти функции берем для реализации FastapiUsers
