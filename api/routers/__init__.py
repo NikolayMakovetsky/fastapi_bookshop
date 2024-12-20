deffec3ce67574ff5ea4ef1574ff0fac58aab82d
NikolayMakovetsky <nicholasmakovetsky@gmail.com>
2024-12-20 22:12:59 +0300

    fix FastApiUsers ImportError in routers\__init__.py

--- api/routers/__init__.py
+++ api/routers/__init__.py
@@ -1,7 +1,7 @@
-from .GenreRouter import router as genre_router
 from .UserRouter import router1 as auth_router
 from .UserRouter import router2 as register_router
 from .AuthTestRouter import router as auth_test_router
+from .GenreRouter import router as genre_router
 from .AuthorRouter import router as author_router
 from .BookRouter import router as book_router
 from .RootRouter import router as root_router
