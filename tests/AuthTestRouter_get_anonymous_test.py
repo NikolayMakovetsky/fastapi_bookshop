e3343de5cabbcb85f307771805da20e0faa7625b
Nikolay Makovetsky <nicholasmakovetsky@gmail.com>
2024-12-22 17:56:16 +0300

    pytest experiments

--- tests/AuthTestRouter_get_anonymous_test.py
+++ tests/AuthTestRouter_get_anonymous_test.py
@@ -10,9 +10,52 @@ TEST_PREFIX = auth_test_router.prefix
 pytestmark = pytest.mark.anyio
 
 
+
 @pytest.fixture
 def anyio_backend():
     return 'asyncio'  # 'trio'
+
+
+USERNAME = "nik@ya.ru"
+PASSWORD = "nik"
+
+async def test_login():
+
+    body_str = f'grant_type=password&username={USERNAME}&password={PASSWORD}&scope=&client_id=string&client_secret=string'
+
+    headers = {
+        "accept": "application/json",
+        "accept-language": "ru-RU",
+        "Content-Type": "application/x-www-form-urlencoded"
+   }
+    async with AsyncClient(
+            transport=ASGITransport(app=application()), base_url=TEST_URL
+    ) as ac:
+        response = await ac.post(f"{TEST_URL}/login", json=body_str, headers=headers)
+    assert response.status_code == 204
+
+
+
+# @pytest.mark.asyncio(forbid_global_loop=True)
+async def test_existent_author3():
+    async with AsyncClient(
+            transport=ASGITransport(app=application()), base_url=TEST_URL
+    ) as ac:
+        response = await ac.get("/authors/1")
+    res = response.json()
+    assert response.status_code == 200
+    assert res['name_author'] == 'Булгаков М.А.'
+
+
+# @pytest.mark.asyncio(forbid_global_loop=True)
+async def test_existent_genre():
+    async with AsyncClient(
+            transport=ASGITransport(app=application()), base_url=TEST_URL
+    ) as ac:
+        response = await ac.get("/genres/1")
+    res = response.json()
+    assert response.status_code == 200
+    assert res['name_genre'] == 'Роман'
 
 
 async def test_anonymous():
@@ -22,3 +65,23 @@ async def test_anonymous():
         response = await ac.get("/anonymous")
     assert response.status_code == 200
     assert response.json() == {'result': 'Hello, Anonymous!'}
+
+# @pytest.mark.asyncio(forbid_global_loop=True)
+async def test_anonymous1():
+    async with AsyncClient(
+            transport=ASGITransport(app=application()), base_url=TEST_URL + TEST_PREFIX
+    ) as ac:
+        response = await ac.get("/anonymous")
+    assert response.status_code == 200
+    assert response.json() == {'result': 'Hello, Anonymous!'}
+
+
+# @pytest.mark.asyncio(forbid_global_loop=True)
+async def test_existent_author2():
+    async with AsyncClient(
+            transport=ASGITransport(app=application()), base_url=TEST_URL
+    ) as ac:
+        response = await ac.get("/authors/1")
+    res = response.json()
+    assert response.status_code == 200
+    assert res['name_author'] == 'Булгаков М.А.'
--- tests/AuthorRouter_get_item_by_id_test.py
+++ tests/AuthorRouter_get_item_by_id_test.py
@@ -1,3 +1,7 @@
+import asyncio
+import time
+
+import pytest
 from fastapi.testclient import TestClient
 
 from api import application
@@ -14,25 +18,60 @@ client = TestClient(application())
 #     assert response.json() == {"Hello": "World"}
 
 
-def test_existent_author():
-    response = client.get(f"{TEST_URL}{TEST_PREFIX}/1")
-    assert response.status_code == 200
-    assert response.json() == {
-        'date_created': '2024-11-12T16:51:14.285878Z',
-        'date_modified': None,
-        'id': 1,
-        'name_author': 'Булгаков М.А.',
-        'row_version': 0,
-        'user_created': 0,
-        'user_modified': None
-    }
+# def test_existent_author():
+#     response = client.get(f"{TEST_URL}{TEST_PREFIX}/1")
+#     assert response.status_code == 200
+#     assert response.json() == {
+#         'date_created': '2024-11-12T16:51:14.285878Z',
+#         'date_modified': None,
+#         'id': 1,
+#         'name_author': 'Булгаков М.А.',
+#         'row_version': 0,
+#         'user_created': 0,
+#         'user_modified': None
+#     }
 
 
-def test_existent_author2():
-    response = client.get(f"{TEST_URL}{TEST_PREFIX}/1")
-    res = response.json()
-    assert response.status_code == 200
-    assert res['name_author'] == 'Булгаков М.А.'
+# def test_login():
+#
+#     USERNAME = "nik@ya.ru"
+#     PASSWORD = "nik"
+#
+#     body_str = f'grant_type=password&username={USERNAME}&password={PASSWORD}&scope=&client_id=string&client_secret=string'
+#
+#     headers = {
+#         "accept": "application/json",
+#         "accept-language": "ru-RU",
+#         "Content-Type": "application/x-www-form-urlencoded"
+#    }
+#
+#     response = client.post(f"{TEST_URL}/login", json=body_str, headers=headers)
+#     assert response.status_code == 204
+
+# @pytest.fixture
+# def event_loop():
+#     loop = asyncio.get_event_loop()
+#     yield loop
+#     loop.close()
+#
+#
+#
+# def test_existent_author2():
+#     response = client.get(f"{TEST_URL}{TEST_PREFIX}/1")
+#     res = response.json()
+#     time.sleep(10)
+#     assert response.status_code == 200
+#     assert res['name_author'] == 'Булгаков М.А.'
+#
+#
+#
+# def test_existent_author3():
+#     response = client.get(f"{TEST_URL}{TEST_PREFIX}/1")
+#     res = response.json()
+#     time.sleep(10)
+#     assert response.status_code == 200
+#     assert res['name_author'] == 'Булгаков М.А.'
+
 
 # def test_non_existent_author():
 #     response = client.get(f"{TEST_URL}{TEST_PREFIX}/999")
