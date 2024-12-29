83294d70ab278d9091d809213d1fda4758cc395e
Nikolay Makovetsky <nicholasmakovetsky@gmail.com>
2024-12-29 17:58:25 +0300

    changed path in load_localize_data() for correct test work

--- api/core/localizators/localizator.py
+++ api/core/localizators/localizator.py
@@ -9,12 +9,12 @@ supported_langs = ['en', 'ru']
 def load_localize_data():
     for lang in supported_langs:
         try:
-            with open(f'./core/resources/localize/messages.{lang}.json', encoding='utf-8') as file:
+            with open(f'../api/core/resources/localize/messages.{lang}.json', encoding='utf-8') as file:
                 data = json.load(file)
                 for k, v in data.items():
                     translations[(lang, k)] = v
         except:
-            logger.error(f'ERROR: File "./core/resources/localize/messages.{lang}.json" not found.')
+            logger.error(f'ERROR: File "../core/resources/localize/messages.{lang}.json" not found.')
 
 
 def get_localize_text(msg_key: str) -> str:
