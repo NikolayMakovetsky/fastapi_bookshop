ffcdc843eeeefe7174f6880829044845d8233721
Nikolay Makovetsky <nicholasmakovetsky@gmail.com>
2025-01-05 20:53:10 +0300

    added DB CONSTANTS import for connection string

--- api/database.py
+++ api/database.py
@@ -3,18 +3,8 @@ from fastapi import Depends
 from fastapi_users.db import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
-from api.core.logging import logger
 from api.models import User
-
-# engine = create_async_engine("sqlite+aiosqlite:///bookshop_db.db")
-# db_connection_string = "postgresql+asyncpg://postgres:postgres@localhost:5432/bookshop_db2"
-
-DB_DRIVER = "postgresql+asyncpg"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "bookshop_db2"
-DB_USER = "postgres"
-DB_PASS = "postgres"
+from config import DB_DRIVER, DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS
 
 
 class AppSession:
